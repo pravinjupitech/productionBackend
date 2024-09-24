9209358d19297cfe25ac4918f993241fc20fe74a
bhawana <bhawanajupitech03@gmail.com>
2024-09-24 14:37:21 +0530

    24-sep

--- controller/steps.controller.js
+++ controller/steps.controller.js
@@ -3,43 +3,19 @@ import { StepsModel } from "../model/steps.model.js";
 export const createSteps = async (req, res, next) => {
   try {
     const { createdBy, steps } = req.body;
-
     const findSteps = await StepsModel.findOne({ createdBy });
-
     if (findSteps) {
-      if (req.body.innerId) {
-        const innerId = req.body.innerId;
-        const findIndex = findSteps.steps.findIndex(
-          (item) => item._id.toString() === innerId
-        );
-
-        if (findIndex !== -1) {
-          findSteps.steps[findIndex] = {
-            ...findSteps.steps[findIndex]._doc,
-            ...req.body,
-          };
-          await findSteps.save();
-          return res
-            .status(200)
-            .json({ message: "Data Updated Successfully", status: true });
-        } else {
-          return res
-            .status(404)
-            .json({ message: "Step with innerId not found", status: false });
-        }
+      if (steps.length > 0) {
+        steps.forEach((step) => {
+          findSteps.steps.push(step);
+        });
       } else {
-        if (Array.isArray(steps)) {
-          steps.forEach((step) => {
-            findSteps.steps.push(step);
-          });
-        } else {
-          findSteps.steps.push(steps);
-        }
-        await findSteps.save();
-        return res
-          .status(200)
-          .json({ message: "Steps Added Successfully", status: true });
+        return res.status(404).json({ message: "Invalid Data", status: false });
       }
+        await findSteps.save();
+      return res
+        .status(200)
+        .json({ message: "Steps Added Successfully", status: true });
     } else {
       const stepsDoc = await StepsModel.create(req.body);
       return stepsDoc
