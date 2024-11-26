2abdd084883f0d1f4c77777890f2ed532d75bd35
bhawana <bhawanajupitech03@gmail.com>
2024-11-26 16:45:54 +0530

    updted

--- controller/assignProduction.controller.js
+++ controller/assignProduction.controller.js
@@ -1006,7 +1006,7 @@ export const deleteProducted = async (req, res, next) => {
     res.status(500).json({ error: "Internal Server Error", status: false });
   }
 };
-// window.history.back()
+// window.history.back()  i wa
 export const demoProduct = async (req, res, next) => {
   try {
     let arr = [1, 2, 3, 3, 4, 5, 5, 5];
