8e021b2bbcb536ddfdec20d6e3c213858a4303f9
bhawana <bhawanajupitech03@gmail.com>
2024-12-20 12:54:13 +0530

    updt

--- controller/warehouse.controller.js
+++ controller/warehouse.controller.js
@@ -147,7 +147,7 @@ export const ViewWarehouseById = async (req, res, next) => {
   try {
     let warehouse = await Warehouse.findById({ _id: req.params.id })
       .sort({ sortorder: -1 })
-      .populate({ path: "productItems.productId", model: "product" });
+      .populate({ path: "productItems.productId", model: "rowProduct" });
     return warehouse
       ? res.status(200).json({ Warehouse: warehouse, status: true })
       : res.status(404).json({ error: "Not Found", status: false });
