522cc907640c30734750a84eee821ac809660215
bhawana <bhawanajupitech03@gmail.com>
2024-12-26 17:39:26 +0530

    stock

--- model/category.model.js
+++ model/category.model.js
@@ -1,56 +1,59 @@
 import mongoose from "mongoose";
 
-const categorySchema = new mongoose.Schema({
+const categorySchema = new mongoose.Schema(
+  {
     id: {
-        type: String
+      type: String,
     },
     created_by: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "user",
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
     },
     database: {
-        type: String
+      type: String,
     },
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     image: {
-        type: String
+      type: String,
     },
     description: {
-        type: String
+      type: String,
     },
     status: {
-        type: String,
-        default: "Active"
+      type: String,
+      default: "Active",
     },
     subcategories: [
-        {
-            id: {
-                type: String
-            },
-            created_by: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "user",
-            },
-            name: {
-                type: String,
-            },
-            image: {
-                type: String
-            },
-            description: {
-                type: String
-            },
-            unitType: {
-                type: String
-            },
-            status: {
-                type: String
-            }
-        }
+      {
+        id: {
+          type: String,
+        },
+        created_by: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "user",
+        },
+        name: {
+          type: String,
+        },
+        image: {
+          type: String,
+        },
+        description: {
+          type: String,
+        },
+        unitType: {
+          type: String,
+        },
+        status: {
+          type: String,
+        },
+      },
     ],
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
 export const Category = mongoose.model("category", categorySchema);
