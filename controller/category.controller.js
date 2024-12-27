476870fefb2cc7fae0942d9dbe8b5b7180c1479f
bhawana <bhawanajupitech03@gmail.com>
2024-12-27 12:28:41 +0530

    stockUpdt

--- controller/category.controller.js
+++ controller/category.controller.js
@@ -3,6 +3,7 @@ import { User } from "../model/user.model.js";
 
 export const saveCategory = async (req, res) => {
   try {
+    console.log("reqbody", req.body);
     const user = await User.findById({ _id: req.body.created_by });
     if (!user) {
       return res.status(400).json({ message: "User Not Found", status: false });
